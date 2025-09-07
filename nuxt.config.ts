b6fc9a0a25f1e9cf7d84937be71a0d85de19280a
isho-0 <isho010607@gmail.com>
2025-09-07 23:27:40 +0900

    docs: 코드 구조 개편
    기존 프론트 코드 제거

--- nuxt.config.ts
+++
@@ -1,9 +0,0 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  modules: ["nuxt-quasar-ui"],
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
-  css: [
-    '~/assets/main.css'
-  ]
-})
