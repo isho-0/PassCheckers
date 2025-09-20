15352d0e069b9d8ad269644a83bcdc45f28e73cc
isho-0 <isho010607@gmail.com>
2025-09-20 13:41:51 +0900

    feat: 외부 접속 기능 추가

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -5,5 +5,33 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   css: [
     '~/assets/main.css'
-  ]
+  ],
+  devServer: {
+    port: 80,
+    host: '0.0.0.0'
+  },
+  router: {
+    options: {
+      scrollBehaviorType: 'smooth'
+    }
+  },
+  vite: {
+    server: {
+      hmr: {
+        port: 80
+      },
+      host: '0.0.0.0',
+      allowedHosts: [
+        'passcheckers.kro.kr',
+        'localhost',
+        '127.0.0.1'
+      ]
+    },
+    logLevel: 'error' // 경고 메시지 숨기기
+  },
+  vue: {
+    compilerOptions: {
+      isCustomElement: (tag) => false
+    }
+  }
 })
